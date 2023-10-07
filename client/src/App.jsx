bd9159be4a78b9601f2782c2ce95f1497cf47c51
Hafizh24 <hafizhcbs@gmail.com>
2023-10-07 14:57:13 +0700

    add dependencies material-tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import MyCarousel from "./components/Carousel";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MyCarousel />
+    </>
+  );
+}
+
+export default App;
