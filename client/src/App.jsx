70ae2b364cf8ca4824045a914b2bc2c0a0b15fd8
wildaniarrachm <132856014+wildaniarrachm@users.noreply.github.com>
2023-10-15 14:35:38 +0700

    feat:Register Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import MyCarousel from "./components/Carousel";
 
 import Navbar from "./components/Navbar";
+import { Register } from "./components/register";
+import { LogIn } from "./components/login";
+
+
+
+
 
 function App() {
   return (
     <>
       <Navbar />
       <MyCarousel />
+      <LogIn />
+      <Register />
+    
     </>
   );
 }
