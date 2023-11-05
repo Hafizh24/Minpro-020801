fbc8d23e877099d89fd62917e41ffa721680129b
Hafizh24 <hafizhcbs@gmail.com>
2023-11-05 15:39:00 +0700

    feat: add controller event

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -76,8 +76,7 @@ const EventCard = ({ formik }) => {
           </div>
           <br />
         </CardHeader>
-       
-          
+
         <CardBody>
           <Input
             name="eventName"
